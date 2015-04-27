3e34288a3aebc6cc1454577c4e27a5f8cc47bc58
gmart020 <gmart020>
2015-04-27 22:51:54 +0000

    Final copy of lab 3 work for submission

--- lab3.h
+++ lab3.h
@@ -1,3 +1,19 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// Name: Gerardo Martinez
+/// SID: 861169126
+/// Date: April 27, 2015
+///
+/// TwoStackedFixed objects are composed of two stacked 
+/// allocated in a single array. They begin at either end of
+/// the array and grow towards the middle. However they may 
+/// not exceed the maximum partition which is inputed by the
+/// user through the constructor. TwoStacksOptimal is the 
+/// same except that the stacks can increase in size up 
+/// until they reach the other stack. The tops of both 
+/// arrays are stored in integers which are initialized to 
+/// -1 for the front array and size for the back array.
+//  ================== END ASSESSMENT HEADER ===============
+
 #ifndef LAB3_H
 #define LAB3_H
 
@@ -12,17 +28,13 @@ template <typename T>
 class TwoStackFixed
 {
     private:
-        stack<T> stack1;
-        stack<T> stack2;
         int sz;
         T *array = NULL;
         int partition;
         int top1;
         int top2;
         
-    public:
-    // if maxtop < 0; array = stack2;
-    // if maxtop >= size; arr = stack1;
+    public:   
         TwoStackFixed(int size, int maxtop)
         {
             if (maxtop < 0 || maxtop > size)
@@ -33,46 +45,202 @@ class TwoStackFixed
             sz = size;
             array = new T[sz];
             partition = maxtop;
-            top1 = 0;
-            top2 = sz - 1;
+            top1 = -1;
+            top2 = sz;
         }
         
         void pushStack1(T value)
         {
-            if (top1 == partition)
+            if (isFullStack1())
             {
                 cout << "Stack overflow" << endl;
                 exit(0);
             }
             array[top1] = value;
-            ++top1;
+            display();
         }
         
         void pushStack2(T value)
         {
-            if (top2 < partition)
+            if (isFullStack2())
             {
                 cout << "Stack overflow" << endl;
                 exit(0);
             }
-            array[top2] = value;
             --top2;
+            array[top2] = value;
+            display();
+        }
+        
+        T popStack1()
+        {
+            if (isEmptyStack1())
+            {
+                cout << "Stack 1 is empty. Nothing to pop.\n";
+                exit(0);
+            }
+            T data = array[top1];
+            --top1;
+            return data;
+        }
+        
+        T popStack2()
+        {
+            if (isEmptyStack2())
+            {
+                cout << "Stack 2 is empty. Nothing to pop.\n";
+                exit(0);
+            }
+            T data = array[top2];
+            ++top2;
+            return data;
         }
         
         void display()
         {
             for (int i = 0; i < sz; ++i)
             {
-                if (i >= top1 && i <= top2)
+                if (i > top1 && i < top2)
                 {
-                    cout << " " << endl;
+                    cout << "  ";
                 }
-                cout << array[i] << " ";
+                else
+                {
+                    cout << array[i] << " ";
+                }
             }
             cout << endl;
         }
         
+        bool isFullStack1()
+        {
+            if (++top1 == partition)
+            {
+                --top1;
+                return true;
+            }
+            return false;
+        }
+        
+        bool isFullStack2()
+        {
+            return (top2 == partition);
+        }
+        
+        bool isEmptyStack1()
+        {
+            return (top1 == -1);
+        }
+        
+        bool isEmptyStack2()
+        {
+            return (top2 == sz);
+        }
         
 };
 
+template <typename T>
+
+class TwoStackOptimal
+{
+    private:
+        int sz;
+        T *array;
+        int top1, top2;
+        
+    public:
+        TwoStackOptimal(int size)
+        {
+            sz = size;
+            array = new T[sz];
+            top1 = -1;
+            top2 = sz;
+        }
+        
+        void pushFlexStack1(T value)
+        {
+            if (isFullStack1())
+            {
+                cout << "Stack is full. Unable to push item.\n";
+                --top1;
+                return;
+            }
+            array[top1] = value;
+            display();
+        }
+        
+        void pushFlexStack2(T value)
+        {
+            if (isFullStack2())
+            {
+                cout << "Stack is full. Unable to push item.\n";
+                ++top2;
+                return;
+            }
+            array[top2] = value;
+            display();
+        }
+        
+        T popFlexStack1()
+        {
+            if (isEmptyStack1())
+            {
+                cout << "Stack is empty. Nothing to pop.\n";
+                exit(0);
+            }
+            T data = array[top1];
+            --top1;
+            return data;
+        }
+        
+        T popFlexStack2()
+        {
+            if (isEmptyStack2())
+            {
+                cout << "Stack is empty. Nothing to pop.\n";
+                exit(0);
+            }
+            T data = array[top2];
+            ++top2;
+            return data;
+        }
+        
+        bool isFullStack1()
+        {
+            if (++top1 == top2) return true;
+            return false;
+        }
+        
+        bool isFullStack2()
+        { 
+            if (--top2 == top1) return true;
+            return false;
+        }
+        
+        bool isEmptyStack1()
+        {
+            return (top1 == -1);
+        }
+        
+        bool isEmptyStack2()
+        {
+            return (top2 == sz);
+        }
+        
+        void display()
+        {
+            for (int i = 0; i < sz; ++i)
+            {
+                if (i > top1 && i < top2)
+                {
+                    cout << "  ";
+                }
+                else
+                {
+                    cout << array[i] << " ";
+                }
+            }
+            cout << endl;
+        }
+};
 #endif
--- main.cc
+++ main.cc
@@ -1,19 +1,128 @@
+//  =============== BEGIN ASSESSMENT HEADER ================
+/// Name: Gerardo Martinez
+/// SID: 861169126
+/// Date: April 27, 2015
+///
+/// This file contains the showTowerStates function which
+/// solves the Towers of Hanoi puzzle recursively while 
+/// printing out every move made. Also contained in this 
+/// file is a test harness for the TwoStackFixed and 
+/// TwoStackedOptimal classes in the lab3 header file.
+//  ================== END ASSESSMENT HEADER ===============
+
+
 #include "lab3.h"
+
+template <typename T>
+
+void showTowerStates(int n, stack<T>& A, stack<T>& B, stack<T>& C)
+{
+    if (n == 1)
+    {
+        cout << "Moved " << A.top() << " from peg A to B.\n";
+        B.push(A.top());
+        A.pop();
+    }
+    else
+    {
+        showTowerStates(n - 1, A, C, B);
+        cout << "Moved " << A.top() << " from peg A to B.\n";
+        B.push(A.top());
+        A.pop();
+        showTowerStates(n - 1, C, B, A);
+    }
+}
 
 int main()
 {
-    TwoStackFixed<int> l1 = TwoStackFixed<int>(6,3);
-    l1.pushStack1(3);
-    //l1.display();
-    l1.pushStack1(4);
-    //l1.display();
-    l1.pushStack1(5);
-    //l1.display();
-    l1.pushStack2(6);
-    //l1.display();
-    l1.pushStack2(7);
-    //l1.display();
-    l1.pushStack2(8);
-    l1.display();
+    stack<int> stack1;
+    stack1.push(5);
+    stack1.push(4);
+    stack1.push(3);
+    stack1.push(2);
+    stack1.push(1);
+    
+    stack<int> stackEnd;
+    stack<int> stackAux;
+    
+    showTowerStates(5, stack1, stackEnd, stackAux);
+    
+    cout << endl << endl;
+    while (stackEnd.size() != 0)
+    {
+        cout << stackEnd.top() << endl;
+        stackEnd.pop();
+    }
+
+    // TwoStackFixed<int> l1 = TwoStackFixed<int>(6,3);
+    // l1.pushStack1(3);
+    // l1.display();
+    // l1.pushStack1(4);
+    // l1.display();
+    // l1.pushStack1(5);
+    // l1.display();
+    // l1.pushStack2(6);
+    // l1.display();
+    // l1.pushStack2(7);
+    // l1.display();
+    // l1.pushStack2(8);
+    // cout << l1.isFullStack1() << endl;
+    // cout << l1.isFullStack2() << endl;
+    // cout << l1.isFullStack1() << endl;
+    // cout << l1.isFullStack2() << endl;
+    // cout << l1.isFullStack1() << endl;
+    // cout << l1.isFullStack2() << endl;
+    
+    // TwoStackFixed<int> l2 = TwoStackFixed<int>(4,2);
+    // cout << l2.isEmptyStack1() << endl;
+    // l2.pushStack1(4);
+    // cout << l2.isEmptyStack1() << endl;
+    // cout << l2.isEmptyStack2() << endl;
+    // l2.pushStack2(5);
+    // cout << l2.isEmptyStack1() << endl;
+
+    
+    // TwoStackOptimal<int> list = TwoStackOptimal<int>(20);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack1(4);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    // list.pushFlexStack2(6);
+    
+    // cout << list.popFlexStack1() << endl;
+    // list.popFlexStack1();
+    // list.popFlexStack1();
+    // list.popFlexStack1();
+    // list.popFlexStack1();
+    // list.pushFlexStack2(9);
+    // list.pushFlexStack2(9);
+    // list.pushFlexStack2(9);
+    // list.pushFlexStack2(9);
+
+
+
     return 0;
 }
